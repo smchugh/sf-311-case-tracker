3cf8e75b60a6198e86cda74e7d178f30fbdde94a
Steve McHugh <smchugh@skillz.com>
2014-05-03 15:12:16 -0400

    Initialize Project

---
+++ CaseTracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaseTracker::Application.config.secret_key_base = '15fbb7d2715eb03e104e0901fa8f445fa7598d2d83b5c8bbc492b610b4deb2beb2346ffb88e78ce0ea1fa677dd51b7e3e10859480b7743aaad8684fa43050bb7'
